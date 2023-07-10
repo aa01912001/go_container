0141ecf576d38937944083730b421641f0391520
Nick <aa01912001@gmail.com>
2023-07-10 20:34:19 +0800

    add testing of stack pop into stack_test.go

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,6 +1,9 @@
 package stack
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestPush(t *testing.T) {
 	type testCase[T any] struct {
@@ -29,7 +32,45 @@ func TestPush(t *testing.T) {
 			stk.Push(v)
 		}
 		if stk.Top() != tc.want {
-			t.Errorf("set value =  %v, but want value = %v", tc.set, tc.want)
+			t.Errorf("want =  %v, but result = %v", tc.want, stk.Top())
+		}
+	}
+}
+
+func TestPop(t *testing.T) {
+	type testCase[T any] struct {
+		set  []T
+		want []T
+	}
+
+	testCases := []testCase[interface{}]{
+		{
+			set:  []interface{}{3, 1, 6, 5, 1},
+			want: []interface{}{1, 5, 6, 1, 3},
+		},
+		{
+			set:  []interface{}{1.1, 2.2, 3.3, 4.4, 5.5},
+			want: []interface{}{5.5, 4.4, 3.3, 2.2, 1.1},
+		},
+		{
+			set:  []interface{}{"a", "b", "c", "d", "e"},
+			want: []interface{}{"e", "d", "c", "b", "a"},
+		},
+	}
+
+	stk := Init[interface{}]()
+	for _, tc := range testCases {
+		for _, v := range tc.set {
+			stk.Push(v)
+		}
+
+		popItems := []interface{}{}
+		for !stk.Empty() {
+			popItems = append(popItems, stk.Top())
+			stk.Pop()
+		}
+		if !reflect.DeepEqual(popItems, tc.want) {
+			t.Errorf("want =  %v, but result = %v", tc.want, popItems)
 		}
 	}
 }
